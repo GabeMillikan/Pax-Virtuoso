be472ec1b6e4ceab57a49ac17c93fc9bec8c0a7a
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2023-09-26 20:08:38 -0700

    add view-logs command

--- abilities/development.py
+++ abilities/development.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import asyncio
 import io
 import subprocess
 import sys
+from pathlib import Path
 
-from discord import File, Interaction, Object
+from discord import File, Interaction, Object, app_commands
 
 from bot import client, tree
 from config import dev_guild_id
@@ -60,3 +63,39 @@ async def reboot(interaction: Interaction) -> None:
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
+
+
+LOG_FILES = [
+    Path("cron") / "stdout.log",
+    Path("cron") / "stderr.log",
+]
+
+
+@tree.command(
+    name="view-logs",
+    description="Shuts down and re-starts the bot.",
+    guild=Object(dev_guild_id),
+)
+@app_commands.choices()
+async def view_logs(interaction: Interaction, line_count: int | None = None) -> None:
+    await interaction.response.defer(ephemeral=True)
+
+    files: list[File] = []
+
+    for log_file in LOG_FILES:
+        with log_file.open("rb") as f:
+            if line_count is None:
+                data = f.read()
+            else:
+                last_n_lines: list[bytes] = []
+
+                for line in f:
+                    last_n_lines.append(line)
+                    if len(last_n_lines) > line_count:
+                        del last_n_lines[0]
+
+                data = b"".join(last_n_lines)
+
+            files.append(File(io.BytesIO(data), filename=log_file.name))
+
+    await interaction.followup.send(files=files)
